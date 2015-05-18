2eb244d6a1f6b7ef48c7c320221b24ea52d9a7e8
James Womack <jwomack@netflix.com>
2015-05-18 10:19:35 -0700

    Add caching

--- index.js
+++ index.js
@@ -1,13 +1,66 @@
-module.exports = function(anArrayOfKeys){
+var Util = require('util'),
+    LRU  = require('lru-cache')
+
+var WHITESPACE_X = /\s+/g
+
+function vaccuum(string) {
+  return string.replace(WHITESPACE_X, '')
+}
+
+function hash(object) {
+  return vaccuum(Util.inspect(object, {
+          depth:      Infinity,
+          showHidden: true
+        }))
+}
+
+var cache = LRU(100)
+
+function validate(anArrayOfKeys) {
   var moduleOutputPrefix = '[keykey]';
 
   var isArray = Array.isArray(anArrayOfKeys);
   if(!isArray){
     throw new TypeError(moduleOutputPrefix + ' Must call with an array.');
   }
+}
 
-  return anArrayOfKeys.reduce(function(result, key){
-    result[key] = key;
-    return result;
-  }, {});
-};
+function reduce(anArrayOfKeys, each) {
+  var subObject = {}
+  anArrayOfKeys.forEach(each.bind(each, subObject, anArrayOfKeys))
+  return subObject
+}
+
+function set(anArrayOfKeys) {
+  var key = hash(anArrayOfKeys)
+  return cache.set(key, anArrayOfKeys)
+}
+
+function create(anArrayOfKeys, bypassCache){
+  validate(anArrayOfKeys)
+  var creation = reduce(anArrayOfKeys, function(result, anArrayOfKeys, key){
+    result[key] = key
+  })
+  !bypassCache && (set(creation))
+  return creation
+}
+
+function get(anArrayOfKeys) {
+  var key = hash(anArrayOfKeys)
+  var object = cache.get(key)
+  if(!object) {
+    return create(anArrayOfKeys)
+  }
+  return object
+}
+
+
+function KeyKey(anArrayOfKeys, bypassCache){
+  return bypassCache ? create(anArrayOfKeys, bypassCache) : get(anArrayOfKeys)
+}
+
+module.exports = KeyKey
+
+module.exports.reset = function () {
+  return cache.reset()
+}
--- test/test.js
+++ test/test.js
@@ -12,10 +12,49 @@ describe('keykey', function(){
 
   it('should return a mirror when what\'s passed is an array', function(){
     expect(keykey([null, '', 1, {}])).to.eql({
-      "null": null,
-      "": "",
-      "1": 1,
-      "[object Object]": {}
+      'null': null,
+      '': '',
+      '1': 1,
+      '[object Object]': {}
     });
+  });
+
+  it('should have a reset function', function(){
+    expect(keykey.reset).to.be.a.function
+  })
+
+  it('should be faster on the second retrieval', function(){
+    var arrayOfKeys,
+        times = {},
+        lastTime = 0
+
+    function start() {
+        lastTime = process.hrtime()
+    }
+
+    function getElapsedTime(key){
+      times[key] = times[key] || 0
+
+      // 3 decimal places
+      var precision = 3;
+      // divide by a million to get nano to milli
+      var elapsed = process.hrtime(lastTime)[1] / 1000000;
+      // print message + time
+
+      times[key] += elapsed.toFixed(precision)
+      return times[key]
+    }
+
+    arrayOfKeys = [null, '', 1, {}]
+
+    start()
+    keykey(arrayOfKeys)
+    getElapsedTime('firstRun')
+
+    start()
+    keykey(arrayOfKeys)
+    getElapsedTime('cached')
+
+    expect(times.firstRun).to.be.above(times.cached)
   });
 });
